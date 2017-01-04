94c4fca51fa51afd8177a333788b6e46b8878702
ler-theo <lerquet.theo@orange.fr>
2017-01-04 17:01:19 +0100

    Add du log corriger

--- classe/Log.php
+++ classe/Log.php
@@ -1,22 +1,27 @@
 <?php
 class Log
 {
-  public static function writeCSV($e) {
 
+  public static function formatDate($param) {
     //Create DateTime
-    $date = new DateTime();
+    $param = new DateTime();
     //Definition du fuseau horaire
-    $date -> setTimezone(new DateTimeZone('Europe/Paris'));
+    $param -> setTimezone(new DateTimeZone('Europe/Paris'));
 
     $log = array(
 
       //Formatage de la Date
-      "Date" => $date -> format('Y-m-d h:i:s'),
-      //Definition du message
-      "Message" => $e -> getMessage()
+      "Date" => $param -> format('Y-m-d h:i:s'),
     );
+    return $log;
+  }
 
-    $log_file = fopen("./logs/logs_".$date -> format ('d-m-y').".csv", "a+");
+  public static function writeCSV($e) {
+
+    Log::formatDate($date);
+
+
+    $log_file = fopen("./logs/logs_".$log -> format ('d-m-y').".csv", "a+");
     fputcsv($log_file, $log, ",");
     fclose($log_file);
   }
